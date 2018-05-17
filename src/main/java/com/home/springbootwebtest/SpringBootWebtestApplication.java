19a0f50d05ee6df867cbe2d51b69f5c16b6b4a8f
Gayal <gkasunjith@gmail.com>
2018-05-17 15:11:33 +0530

    origin/project

---
+++ src/main/java/com/home/springbootwebtest/SpringBootWebtestApplication.java
@@ -0,0 +1,12 @@
+package com.home.springbootwebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebtestApplication.class, args);
+    }
+}
