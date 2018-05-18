b504ace6d229283cd9b9e8ae7f8db785ccd5e1df
Gayal <gkasunjith@gmail.com>
2018-05-18 09:57:25 +0530

    order modified

--- src/main/java/com/home/springbootwebtest/dto/OrderDTO.java
+++ src/main/java/com/home/springbootwebtest/dto/OrderDTO.java
@@ -1,4 +1,42 @@
 package com.home.springbootwebtest.dto;
 
-public class OrderDTO {
+import java.io.Serializable;
+
+public class OrderDTO implements Serializable {
+
+    private int id;
+    private String date;
+
+    public OrderDTO() {
+    }
+
+    public OrderDTO(int id, String date) {
+        this.setId(id);
+        this.setDate(date);
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDTO{" +
+                "id=" + id +
+                ", date='" + date + '\'' +
+                '}';
+    }
 }
